e2e8753fba3ba5bbd630e73b4bf4b4454dc3537f
MohammedAlKilani <120374233+MohammedAlKilani@users.noreply.github.com>
2023-03-25 14:40:16 +0200

    Delete routes directory

--- routes/task_routes.js
+++
@@ -1,9 +0,0 @@
-const express =require("express")
-const router =express.Router()
-const {add_task,all_task,find_task,update_task,delete_task} =require("../controllers/task")
-
-router.route("/").get(all_task).post(add_task)
-
-router.route("/:taskip").get(find_task).patch(update_task).delete(delete_task)
-
-module.exports=router
